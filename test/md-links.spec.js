f148dd8178204a5a26dd9ce1b2dbe4aae5c31d41
Ana <ana_leticia@geb.inatel.br>
2023-04-28 15:37:30 -0300

    finalização dos testes da função mdlinks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -23,4 +23,38 @@ describe('Função mdLinks', () => {
         expect(getLinks).toHaveBeenCalledTimes(2);
       });
     });
+
+    it('deve chamar a função que valida os links recebidos', () => {
+      const mockRead = {};
+      read.mockResolvedValueOnce(mockRead);
+      const mockGetLinks = [];
+      getLinks.mockResolvedValueOnce(mockGetLinks);
+      const mockValidate = [{}, {}];
+      validate.mockResolvedValueOnce(mockValidate);
+
+      const pathFile = 'arquivoqualquer';
+      return mdlinks(pathFile, {validate: true}).then(() => {
+        expect(read).toHaveBeenCalledTimes(1);
+        expect(read).toHaveBeenCalledWith(pathFile);
+        expect(getLinks).toHaveBeenCalledTimes(1);
+        expect(getLinks).toHaveBeenCalledWith(mockRead);
+        expect(validate).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    it('deve resolver a promessa', () => {
+      const mockRead = {};
+      read.mockResolvedValueOnce(mockRead);
+      const mockGetLinks = [];
+      getLinks.mockResolvedValueOnce(mockGetLinks);
+      const pathFile = 'arquivoqualquer';
+
+      return mdlinks(pathFile, {validate: false}).then(() => {
+        expect(read).toHaveBeenCalledTimes(1);
+        expect(read).toHaveBeenCalledWith(pathFile);
+        expect(getLinks).toHaveBeenCalledTimes(1);
+        expect(getLinks).toHaveBeenCalledWith(mockRead);
+        expect(validate).not.toHaveBeenCalled();
+    });
+  })
 });
